9e23bfeebcd244030ed468b6f2103514caabe2e6
HypatiaG <ikhsannul201@gmail.com>
2026-06-16 17:40:29 +0700

    Add race command to display driver stints and integrate with CLI

---
+++ src/commands/race.py
@@ -0,0 +1,43 @@
+import logging
+import typer
+from rich.console import Console
+from rich.table import Table
+from utils.loading import load_session
+
+app = typer.Typer()
+console = Console()
+logging.getLogger("fastf1").setLevel(logging.CRITICAL)
+
+@app.command(help="Display race stint of driver")
+def stint(
+    year: int = typer.Option(...,"--year","-y", help="Year of the season"),
+    round: int = typer.Option(...,"--round","-r", help="Round in the season"),
+    driver: str = typer.Option(...,"--driver","-d", help="Driver's code (e.g. HAM, VER, etc.)")):
+
+    session = load_session(year, round, "R")
+    laps = session.laps
+    driver_name = session.get_driver(driver)["Abbreviation"]
+    console.print(driver_name)
+
+    stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
+    stints = stints.groupby(["Driver", "Stint", "Compound"])
+    stints = stints.count().reset_index()
+
+    stints = stints.rename(columns={"LapNumber": "StintLength"})
+    # console.print(stints[stints["Driver"] == driver_name])
+
+    table = Table()
+    table.add_column("Driver", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Stint", style="white")
+    table.add_column("Compound", justify="right", style="yellow")
+    table.add_column("StintLength", justify="right", style="yellow")
+
+    for _, row in stints[stints["Driver"] == driver_name].iterrows():
+        table.add_row(
+            str(row["Driver"]),
+            str(row["Stint"]),
+            str(row["Compound"]),
+            str(row["StintLength"])
+        )
+
+    console.print(table)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 import typer
-from commands import calendar, session
+from commands import calendar, session, race
 
 app = typer.Typer(help="FastF1 CLI — Formula 1 data at your fingertips")
 
 app.add_typer(session.app, name="session")
 app.add_typer(calendar.app, name="calendar")
+app.add_typer(race.app, name="race")
 
 if __name__ == "__main__":
     app()
